a159d7f705d0242af80b452217e6fa745a697c55
shrikarjagtap <119429380+shrikarjagtap@users.noreply.github.com>
2024-05-25 19:28:47 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Footer = () => (
+    <footer className='flex flex-col center mt-10 mb-5  '>
+      <a
+        href='https://gdsc.community.dev/mit-adt-university-pune-india/'
+        className='link footer__link'
+      >
+  Made with ❤ by SHRIKAR</a>
+    </footer>
+  )
+  
+  export default Footer
+  
